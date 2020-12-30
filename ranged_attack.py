50d547b17e681b8837a2e5e436338d7257faa7a9
Apocalvps <41271617+Apocalvps@users.noreply.github.com>
2020-12-29 19:39:38 -0500

    style consistency

--- ranged_attack.py
+++ ranged_attack.py
@@ -4,16 +4,16 @@ def get_modifier_subtotal(reflex,skill,other):
     return reflex+skill+other
 
 def get_hit_locations(hit_num):
-    hit_table = {1:"Head",
-                 2:"Torso",
-                 3:"Torso",
-                 4:"Torso",
-                 5:"R.Arm",
-                 6:"L.Arm",
-                 7:"R.Leg",
-                 8:"R.Leg",
-                 9:"L.Leg",
-                 10:"L.Leg"}
+    hit_table = {1:'Head',
+                 2:'Torso',
+                 3:'Torso',
+                 4:'Torso',
+                 5:'R.Arm',
+                 6:'L.Arm',
+                 7:'R.Leg',
+                 8:'R.Leg',
+                 9:'L.Leg',
+                 10:'L.Leg'}
     
     hit_locations = []
     
@@ -25,11 +25,11 @@ def get_hit_locations(hit_num):
     return hit_locations
 
 def get_to_hit(distance):
-    to_hit = {"Point Blank":10,
-              "Close":15,
-              "Medium":20,
-              "Long":25,
-              "Extreme":30}
+    to_hit = {'Point Blank':10,
+              'Close':15,
+              'Medium':20,
+              'Long':25,
+              'Extreme':30}
     hit_num = to_hit[distance]
     return hit_num
 
@@ -49,9 +49,9 @@ def reliability_roll(weapon):
     #reliability = weapon.reliability
     reliability = 'ST' #Placeholder
     
-    jam_table = {"VR":3,
-                 "ST":5,
-                 "UR":8}
+    jam_table = {'VR':3,
+                 'ST':5,
+                 'UR':8}
     jam_check = jam_table[reliability]
     
     jam_roll = sum(dice.roll('1d10'))
@@ -72,32 +72,32 @@ def combat_fumble_roll(weapon):
     print('Rolled a {0}'.format(fumble_roll))
     
     if fumble_roll <= 4:
-        print("No fumble. You just screw up.")
+        print('No fumble. You just screw up.')
     elif fumble_roll == 5:
-        print("You drop your weapon.")
+        print('You drop your weapon.')
     elif fumble_roll == 6:
-        print("Weapon discharges or strikes something harmiess.")
+        print('Weapon discharges or strikes something harmiess.')
         reliability_roll(weapon)
     elif fumble_roll == 7:
-        print("Weapon jams or imbeds itself in the ground for one turn.")
+        print('Weapon jams or imbeds itself in the ground for one turn.')
         reliability_roll(weapon)
     elif fumble_roll == 8:
-        print("You manage to to wound yourself. Roll for location.")
+        print('You manage to to wound yourself. Roll for location.')
         ranged_hits(weapon,hit_num=1,aim='No')
     elif fumble_roll <= 10:
-        print("You manage to wound a member of your own party.")
+        print('You manage to wound a member of your own party.')
         ranged_hits(weapon,hit_num=1,aim='No')
     else:
         pass
     return None
 
 def get_single_fire_hits(weapon,rounds,distance,modifier_sum):
-    print("Firing {0} rounds at {1} range with modifier {2}".format(rounds,distance.lower(),modifier_sum))
+    print('Firing {0} rounds at {1} range with modifier {2}'.format(rounds,distance.lower(),modifier_sum))
     to_hit = get_to_hit(distance)
     hit_num = 0
     for i in range(rounds):
         hit_score = sum(dice.roll('1d10'))
-        print("Rolled a "+str(hit_score))
+        print('Rolled a {0}'.format(hit_score))
         if hit_score == 1:
             combat_fumble_roll(weapon)
             break #assuming a critical fail ends the attack even if you have ROF left
@@ -109,18 +109,18 @@ def get_single_fire_hits(weapon,rounds,distance,modifier_sum):
                 hit_num += 1
             else:
                 pass
-    print("Hit {0} rounds".format(hit_num))
+    print('Hit {0} rounds'.format(hit_num))
     return hit_num
 
 def get_three_round_burst_hits(weapon,rounds,distance,modifier_sum):
-    print("Firing 3-round burst at {1} range with modifier {2}".format(rounds,distance.lower(),modifier_sum))
-    if distance == "Long" or distance == "Extreme":
+    print('Firing 3-round burst at {1} range with modifier {2}'.format(rounds,distance.lower(),modifier_sum))
+    if distance == 'Long' or distance == 'Extreme':
         pass
     else:
         modifier_sum += 3
     to_hit = get_to_hit(distance)
     hit_score = sum(dice.roll('1d10'))
-    print("Rolled a "+str(hit_score))
+    print('Rolled a {0}'.format(hit_score))
     if hit_score == 1:
         hit_num = 0
         reliability_roll(weapon)
@@ -132,18 +132,18 @@ def get_three_round_burst_hits(weapon,rounds,distance,modifier_sum):
             hit_num = sum(dice.roll('1d3'))
         else:
             hit_num = 0
-    print("Hit {0} rounds".format(hit_num))
+    print('Hit {0} rounds'.format(hit_num))
     return hit_num
         
 def get_automatic_fire_hits(weapon,rounds,distance,modifier_sum):
-    print("Firing {0} rounds full-auto at {1} range with modifier {2}".format(rounds,distance.lower(),modifier_sum))
-    if distance == "Close" or distance == "Point Blank":
+    print('Firing {0} rounds full-auto at {1} range with modifier {2}'.format(rounds,distance.lower(),modifier_sum))
+    if distance == 'Close' or distance == 'Point Blank':
         modifier_sum += int(rounds/10)
     else:
         modifier_sum += -int(rounds/10)
     to_hit = get_to_hit(distance)
     hit_score = sum(dice.roll('1d10'))
-    print("Rolled a "+str(hit_score))
+    print('Rolled a {0}'.format(hit_score))
     if hit_score == 1:
         hit_num = 0
         reliability_roll(weapon)
@@ -154,24 +154,24 @@ def get_automatic_fire_hits(weapon,rounds,distance,modifier_sum):
         hit_num = hit_score - to_hit
         hit_num_bounded = lambda x, l, u: l if x < l else u if x > u else x
         hit_num = hit_num_bounded(hit_num,0,rounds)
-    print("Hit {0} rounds".format(hit_num))
+    print('Hit {0} rounds'.format(hit_num))
     return hit_num
     
 def get_suppressive_fire_hits(weapon,rounds,fire_zone_area,suppress_save_modifier):
-    print("Suppressing a {0}-meter area with {1} rounds".format(fire_zone_area,rounds))
+    print('Suppressing a {0}-meter area with {1} rounds'.format(fire_zone_area,rounds))
     suppression_save_check = rounds / fire_zone
     save_roll = sum(dice.roll('1d10'))
-    print("Rolled a "+str(save_roll))
+    print('Rolled a {0}'.format(save_roll))
     save_roll += suppress_save_modifier
     if save_roll >= suppression_save_check:
         hit_num = 0
     else:
         hit_num = sum(dice.roll('1d10'))
-    print("Hit {0} rounds".format(hit_num))
+    print('Hit {0} rounds'.format(hit_num))
     return hit_num
 
 def ranged_hits(weapon,hit_num,aim):
-    if aim=="No":
+    if aim=='No':
         hit_locations = get_hit_locations(hit_num)
     else:
         hit_locations = [aim for i in distance(hit_num)]
@@ -183,25 +183,25 @@ def ranged_hits(weapon,hit_num,aim):
     for i in range(hit_num):
         hit = (hit_locations[i],base_dmgs[i])
         hit_list.append(hit)
-        print("{0} dmg to {1}".format(hit[1],hit[0]))
+        print('{0} dmg to {1}'.format(hit[1],hit[0]))
         
     return hit_list
     
-def attack(weapon,firing_mode,rounds,distance,modifier_subtotal="0",aim="No",suppress_save_modifier="0",fire_zone_area="2"):
+def attack(weapon,firing_mode,rounds,distance,modifier_subtotal='0',aim='No',suppress_save_modifier='0',fire_zone_area='2'):
     #modifier_subtotal += weapon.wa
     
-    if aim=="No":
+    if aim=='No':
         modifier_sum = modifier_subtotal
     else:
         modifier_sum = modifier_subtotal - 4
         
-    if firing_mode == "Automatic":
+    if firing_mode == 'Automatic':
         hit_num = get_automatic_fire_hits(weapon,rounds,distance,modifier_sum)
-    elif firing_mode == "Burst":
+    elif firing_mode == 'Burst':
         hit_num = get_three_round_burst_hits(weapon,rounds,distance,modifier_sum)
-    elif firing_mode == "Single":
+    elif firing_mode == 'Single':
         hit_num = get_single_fire_hits(weapon,rounds,distance,modifier_sum)
-    elif firing_mode == "Suppress":
+    elif firing_mode == 'Suppress':
         hit_num = get_suppressive_fire_hits(weapon,rounds,fire_zone_area,suppress_save_modifier)
         
     hit_list = ranged_hits(weapon,hit_num,aim)
